d568bd9531ce4608eb56aaa457e1549809e08b55
Shivam30Mishra <theshivammishra10@gmail.com>
2025-12-25 21:15:36 +0530

    understood about the typescript and tsconfig file and how to set up the typescript env

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+let x: number = 1;
+console.log("The type of x is : " + typeof(x));
+
+function greet(name: string): string{
+  return `Hello ${name}!`
+}
+
+function sum(a: number, b: number): number{
+  return a+b;
+}
+
+let a = sum(2,3);
+let s = greet("shivam")
+
+console.log(a)
+console.log(s)
+
+// problem 2 : return true or false if a use is 18+
+function isAdult(age: number): boolean{
+  if(age>=18){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+// problem 3 : create a fxn that takes another fxn as input and return it after 1 second
+function delayedCall(fn: () => void){
+  setTimeout(fn, 1000);
+}
+
+delayedCall(() => {
+  console.log("this is the delayed call")
+})
+
+
+// tsConfig file information : 
+// 1)TARGET : specifies the ECMA type version to which typescript compiler will compile the ts code 
+// to try it outerHeight, try compiling the following code for target being ES5 and ES2020
+// const great = (name: string) => `Hello,${name}`
+
+// 2)ROOTDIR : create a new folder where rootDir is : "./src" where all the ts files will be stored and create the outDir where all the js files created after running npx tsc -b will be stored the compilation ProcessingInstruction
+
+// 3)removeComments : true/false
+
+// 4)noImlicitAny : true indicated if none type is assigned explicitly then value : any will be assigned implicitly
+
